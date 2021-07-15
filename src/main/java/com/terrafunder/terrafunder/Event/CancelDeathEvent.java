8a94ebe1cc41d69d6962ec7c2a8bcdb45e855602
maleyy <63105226+Wongt8@users.noreply.github.com>
2021-07-15 16:28:08 +0200

    Cancel Death

---
+++ src/main/java/com/terrafunder/terrafunder/Event/CancelDeathEvent.java
@@ -0,0 +1,29 @@
+package com.terrafunder.terrafunder.Event;
+
+import com.terrafunder.terrafunder.Terrafunder;
+import com.terrafunder.terrafunder.Timer.TimerTasks;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class CancelDeathEvent implements Listener {
+
+
+    private Terrafunder main;
+
+    public CancelDeathEvent(Terrafunder game){
+        this.main = game;
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent event){
+        if(TimerTasks.day < this.main.CONFIG.getInt("TimeBeforePvp")){
+            Player player = (Player) event.getEntity();
+            if (event.getFinalDamage() >= player.getHealth()){
+                event.setCancelled(true);
+                player.sendMessage("§a>[SERVEUR] Le bon dieu vous fait grâce de votre mort");
+            }
+        }
+    }
+}
